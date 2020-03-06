a9f7d87fe0542912a31e777d2c05f3aec07312f8
Richard Keenan <rskeenan@gmail.com>
2020-03-06 20:49:43 +0000

    get-temp impl

--- functions/get-temp.js
+++ functions/get-temp.js
@@ -1,6 +1,19 @@
-exports.handler = function(event, context, callback) {
+const AWS = require("aws-sdk");
+
+const DynamoDB = new AWS.DynamoDB.DocumentClient({
+  region: "eu-west-2"
+});
+
+exports.handler = async (event, context, callback) => {
+  const data = await DynamoDB.scan({
+    TableName: "temp"
+  }).promise();
+
+  const itemsByTime = data.Items.sort((a, b) => a.time.localeCompare(b.time));
+  const body = itemsByTime.map(i => ({ time: i.time, temp: i.temp }));
+
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body
   });
 };
