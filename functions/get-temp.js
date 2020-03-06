24c301c973bc3f9d693c2f033abe481846567a58
Richard Keenan <rskeenan@gmail.com>
2020-03-06 21:03:34 +0000

    log

--- functions/get-temp.js
+++ functions/get-temp.js
@@ -11,16 +11,19 @@ AWS.config = config;
 const DynamoDB = new AWS.DynamoDB.DocumentClient({});
 
 exports.handler = (event, context, callback) => {
+  console.log("Enter handler");
+
   DynamoDB.scan({
     TableName: "temp"
   })
     .promise()
     .then(data => {
+      console.log("Pulled data from Dynamo", data.Items.length);
       const itemsByTime = data.Items.sort((a, b) =>
         a.time.localeCompare(b.time)
       );
       const body = itemsByTime.map(i => ({ time: i.time, temp: i.temp }));
-
+      console.log("Returning body of ", JSON.stringify(body));
       callback(null, {
         statusCode: 200,
         body
