766913005959236ee9b770b16cb6ef53d2cb9dd0
Richard Keenan <rskeenan@gmail.com>
2020-03-07 01:05:04 +0000

    chart

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from "recharts";
 
 function App() {
+  const [data, setData] = useState<{ time: string; temp: number }[]>([]);
+  useEffect(() => {
+    fetch("/.netlify/functions/get-temp")
+      .then(r => r.json())
+      .then(setData)
+      .catch(err => console.log(err));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <LineChart width={1000} height={500} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" tick={false} />
+        <YAxis domain={["dataMin", "dataMax"]} tickCount={20} />
+        <Tooltip
+          labelFormatter={time =>
+            "Time: " + new Date(time).toLocaleTimeString()
+          }
+        />
+        />
+        <Line
+          isAnimationActive={false}
+          name="Temperature /°C"
+          type="monotone"
+          dataKey="temp"
+          stroke="#8884d8"
+          dot={false}
+        />
+      </LineChart>
     </div>
   );
 }
